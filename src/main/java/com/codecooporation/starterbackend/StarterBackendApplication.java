317c3613b48d3b24d30f7feba656412749f80527
Ahmed <ahmed.mzoughi@infor.com>
2022-05-14 11:12:19 +0100

    allow cors

--- src/main/java/com/codecooporation/starterbackend/StarterBackendApplication.java
+++ src/main/java/com/codecooporation/starterbackend/StarterBackendApplication.java
@@ -2,6 +2,7 @@ package com.codecooporation.starterbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StarterBackendApplication {
 
+	@CrossOrigin(origins = "http://starter-bucket-test.s3-website.eu-central-1.amazonaws.com")
 	@GetMapping("/welcome")
 	public String welcome(){
 		return "say welcome to automation";
 	}
 
+	@CrossOrigin(origins = "http://starter-bucket-test.s3-website.eu-central-1.amazonaws.com")
 	@GetMapping("/health")
 	public String health(){
 		return "your app is up and running";
